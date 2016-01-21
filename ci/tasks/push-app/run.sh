45ada4a4cf988482afce687e20b7b478be357c8f
Stephen Levine <stephen.levine@gmail.com>
2016-01-21 13:43:58 -0500

    Move privileged parameter to correct place in task config

--- ci/tasks/push-app/run.sh
+++ ci/tasks/push-app/run.sh
@@ -3,13 +3,7 @@
 set -ex
 
 domain=local.nanocf.io
-
-resolvconf=$(cat /etc/resolv.conf)
-rm -f /etc/resolv.conf
-echo "$resolvconf" > /etc/resolv.conf
-
 DOCKER_RUN_BLOCKS=false /var/micropcf/docker-run "$domain"
 
 cf push app -o cloudfoundry/lattice-app -m 32M
-
 curl -v "http://app.${domain}"
