3bc0b34d346506088e8e684e3f4317441bbbc556
Stephen Levine <stephen.levine@gmail.com>
2016-01-21 13:07:12 -0500

    Remove unnecessary targetting in CI task

--- ci/tasks/push-app/run.sh
+++ ci/tasks/push-app/run.sh
@@ -10,9 +10,6 @@ echo "$resolvconf" > /etc/resolv.conf
 
 DOCKER_RUN_BLOCKS=false /var/micropcf/docker-run "$domain"
 
-cf api "api.$domain" --skip-ssl-validation
-cf auth admin admin
-cf target -o micropcf-org -s micropcf-space
 cf push app -o cloudfoundry/lattice-app -m 32M
 
 curl -v "http://app.${domain}"
