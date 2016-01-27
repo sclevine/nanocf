45df31d68eeb5284b66531a3061260404a6dc7ee
Stephen Levine <stephen.levine@gmail.com>
2016-01-27 09:59:45 -0500

    Upgrade CI acceptance tests to use nanocf-test command

--- ci/tasks/acceptance/run.sh
+++ ci/tasks/acceptance/run.sh
@@ -2,14 +2,10 @@
 
 set -ex
 
-nanocf local.nanocf
-
 apt-get -qqy install git
 
-git -C nanocf submodule update --init micropcf
-git -C nanocf/micropcf submodule update --init test/src/github.com/cloudfoundry/cf-acceptance-tests
-git -C nanocf/micropcf submodule update --init test/src/github.com/cloudfoundry-incubator/diego-acceptance-tests
-git -C nanocf/micropcf submodule update --init --recursive images/releases/diego-release
+git -C nanocf submodule update --init test/src/github.com/cloudfoundry/cf-acceptance-tests
+ln -sf $PWD/nanocf/test /test
 
-./nanocf/micropcf/bin/cats local.nanocf
-./nanocf/micropcf/bin/dats local.nanocf
+nanocf local.nanocf
+nanocf-test local.nanocf
